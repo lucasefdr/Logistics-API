58a8d98925cd207177785b1372b92f3c1caa5a46
Lucas <lcseferreira@hotmail.com>
2022-10-31 10:27:07 -0300

    First commit

---
+++ src/main/java/com/lucasferreira/logistics/LogisticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasferreira.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasferreira/logistics/LogisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasferreira.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
