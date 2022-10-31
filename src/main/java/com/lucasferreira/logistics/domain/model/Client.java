26bb731a0458aea64cf118389481ba52f2f2d042
Lucas <lcseferreira@hotmail.com>
2022-10-31 10:53:01 -0300

    Create entity Client

---
+++ src/main/java/com/lucasferreira/logistics/domain/model/Client.java
@@ -0,0 +1,21 @@
+package com.lucasferreira.logistics.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Client {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+}
