671cad2eb7a0181d79d5d50656633576560c6130
Lucas <lcseferreira@hotmail.com>
2022-11-03 14:39:10 -0300

    Add Flyway

--- src/main/java/com/lucasferreira/logistics/controller/ClientController.java
+++ src/main/java/com/lucasferreira/logistics/controller/ClientController.java
@@ -3,8 +3,9 @@ package com.lucasferreira.logistics.controller;
 import com.lucasferreira.logistics.domain.model.Client;
 import com.lucasferreira.logistics.service.ClientService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,7 +16,14 @@ public class ClientController {
 
     private final ClientService clientService;
 
-    public List<Client> listAll() {
-        return clientService.listAll();
+    @GetMapping
+    public ResponseEntity<List<Client>> listAll() {
+        return ResponseEntity.ok(clientService.listAll());
     }
+
+    @PostMapping
+    public ResponseEntity<Client> save(@RequestBody Client client) {
+        return new ResponseEntity<>(clientService.save(client), HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/lucasferreira/logistics/service/ClientService.java
+++ src/main/java/com/lucasferreira/logistics/service/ClientService.java
@@ -3,11 +3,15 @@ package com.lucasferreira.logistics.service;
 import com.lucasferreira.logistics.domain.model.Client;
 import com.lucasferreira.logistics.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * <b>Service class</b>: responsável pelas regras de negócio <br>
+ * <b>@Service</b>: componente para indicar que a classe é uma classe de serviço <br>
+ * <b>@RequiredArgsConstructor</b>: responsável por gerar construtor para classes abstratas (final)
+ */
 @Service
 @RequiredArgsConstructor
 public class ClientService {
@@ -15,5 +19,9 @@ public class ClientService {
 
     public List<Client> listAll() {
         return clientRepository.findAll();
+    }
+
+    public Client save(Client client) {
+        return clientRepository.save(client);
     }
 }
